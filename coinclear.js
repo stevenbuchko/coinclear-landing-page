1a67811563e525eaf4f848f97f1933f2e63ade92
jakeBergs <jake.bergal@gmail.com>
2017-09-14 11:26:57 -0600

    added in some basic js stuff for changing highlighted

--- coinclear.js
+++ coinclear.js
@@ -1,7 +1,17 @@
-var signupMain = document.querySelector("#signup-main");
-var signup = document.querySelector("#email-signup");
+// var signupMain = document.querySelector("#signup-main");
+// var signup = document.querySelector("#email-signup");
+var steps = document.querySelectorAll("li>div");
+var highStep = 0;
 
 
-signupMain.addEventListener("click", function(){
-	signup.scrollIntoView();
-});
+// signupMain.addEventListener("click", function(){
+// 	signup.scrollIntoView();
+// });
+
+var i;
+for (i = 0; i < steps.length; i++) {
+	steps[i].addEventListener("click", function() {
+		document.querySelector(".step-highlight").classList.remove("step-highlight");
+		this.classList.add("step-highlight");
+	})
+}
