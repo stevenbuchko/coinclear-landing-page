bb2ce49a9e9a230ca761ca1bd4ddc69dc6b66a59
jakeBergs <jake.bergal@gmail.com>
2017-09-14 22:40:15 -0600

    updated screen to be in line with desired styling

--- coinclear.js
+++ coinclear.js
@@ -1,8 +1,9 @@
 // var signupMain = document.querySelector("#signup-main");
 // var signup = document.querySelector("#email-signup");
-var steps = document.querySelectorAll("li>div");
+var steps = document.querySelectorAll("li");
 var highStep = 0;
-
+var droidImgs = ["assets/CCInfo-Android.png","assets/Portfolio-Android.png","assets/Dashboard-Android.png"];
+var droidScreen = document.querySelector(".step-screen");
 
 // signupMain.addEventListener("click", function(){
 // 	signup.scrollIntoView();
@@ -11,7 +12,12 @@ var highStep = 0;
 var i;
 for (i = 0; i < steps.length; i++) {
 	steps[i].addEventListener("click", function() {
+
 		document.querySelector(".step-highlight").classList.remove("step-highlight");
 		this.classList.add("step-highlight");
+		highStep = parseInt(this.innerText[0]-1)
+		console.log(highStep);
+		droidScreen.src = droidImgs[highStep];
+
 	})
 }
